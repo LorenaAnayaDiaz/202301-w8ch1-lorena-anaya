67063424082427690e7d27b6a4e22e6131038a79
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-07 18:45:44 +0100

    add characters list tests with error handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,3 +6,9 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(charactersResponseFulfilled));
   }),
 ];
+
+export const errorHandlers = [
+  rest.get("https://swapi.dev/api/people", (_req, res, ctx) => {
+    return res.once(ctx.status(429), ctx.json(null));
+  }),
+];
