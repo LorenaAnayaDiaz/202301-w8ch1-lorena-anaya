8e20a7cbf7637990951f19f90af465f11b2ff3f3
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-07 17:33:12 +0100

    add home page and route

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,11 @@
-import { Link } from "react-router-dom";
-import CardList from "../../features/cardlist/CardList";
-
+import CharactersList from "../../features/characters/CharacterList";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <>
-      <h1 className="home__title">Check out the robots!</h1>
-      <CardList />
-      <Link className="text" data-testid="link" to={"/Form"}>
-        CREATE YOUR OWN ROBOTS!
-      </Link>{" "}
+      <h1 className="home__title">CHARACTERS</h1>
+      <CharactersList />
     </>
   );
 };
