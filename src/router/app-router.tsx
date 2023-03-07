24a6b3279e7821a3837416fa73651cb94fedef7d
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-07 17:30:50 +0100

    remove unnecesary form route from app router

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
-import { Form } from "../pages/Form/Form";
 import { Home } from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
 const router = createBrowserRouter([
@@ -13,10 +12,6 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
-      },
-      {
-        path: "/Form",
-        element: <Form />,
       },
     ],
   },
