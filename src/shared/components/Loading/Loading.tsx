b01a72578525a918935ecf45f039c4ed8859cf2f
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-07 17:11:12 +0100

    add loading shared component

---
+++ src/shared/components/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <img
+      src="https://cdn.dribbble.com/users/361263/screenshots/3051905/media/9509e755410f4da6a7dd6e2d26552522.gif"
+      alt="Loading img"
+    />
+  );
+};
+
+export default Loading;
